781de4a4085864799bc497c52219e0afb949a0b9
0xCurtis <curtis1337wastaken@gmail.com>
2024-09-19 05:05:03 +0200

    Edit Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="AizyPy",
-    version="0.1",
+    version="0.2",
     description="Wrapper around Aizy bot methods for local testing and development.",
     author="Aizy Team",
     packages=["aizypy"],
