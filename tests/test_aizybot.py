9a7c3b313735c0c6b6f3480b5cbbebf63afc5905
0xCurtis <curtis1337wastaken@gmail.com>
2024-11-08 00:33:22 +0100

    ✅ new tests

--- tests/test_aizybot.py
+++ tests/test_aizybot.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import MagicMock
-from src.AizyBot import AizyBot, Trade
+from src.AizyBot import AizyBot
+from src.Trade import Trade
 from src.OrderManager import OrderStatus
 
 class TestAizyBot(unittest.IsolatedAsyncioTestCase):
@@ -11,20 +12,19 @@ class TestAizyBot(unittest.IsolatedAsyncioTestCase):
 
     async def test_place_order(self):
         await self.bot.place_order("buy", 1.0, 50000, "BTC/USD", "market")
-        active_orders = self.bot.list_active_orders()
+        active_orders = self.bot.list_active_trades()
         self.assertEqual(len(active_orders), 1)
         print(active_orders)
         self.assertEqual(active_orders[0].status, OrderStatus.ACTIVE)
 
     async def test_close_trade(self):
-        trade = Trade("buy", "1", "10", "BTC/USD")
-        self.bot.active_trades.append(trade)
+        await self.bot.place_order("buy", 1.0, 50000, "BTC/USD", "market")
         
         # Await the close_trade method
-        await self.bot.close_trade(trade)
+        await self.bot.close_trade(self.bot.list_active_trades()[-1])
         
         # Assert that the trade list is empty after closing the trade
-        self.assertEqual(len(self.bot.active_trades), 0)
+        self.assertEqual(len(self.bot.list_active_trades()), 0)
 
 if __name__ == "__main__":
     unittest.main()
