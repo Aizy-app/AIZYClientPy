35b977f4cbced7c1877e3222d3442bda651022a4
0xCurtis <curtis1337wastaken@gmail.com>
2025-01-16 23:09:51 +0100

    edited workflow and setup to publish

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="AizyPy",
     version="0.3",
     description="Wrapper around Aizy bot methods for local testing and development.",
     author="Aizy Team",
-    packages=["aizypy"],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "python-dateutil>=2.8.2",
+        "pytz>=2023.3",
+        "asyncio>=3.4.3",
+        "typing-extensions>=4.7.1",
+    ],
+    python_requires=">=3.7",
 )
